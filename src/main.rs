4615d89056a7bfc0ac8fbb76bb354995436c9652
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2019-04-17 19:59:04 -0400

    Add ASCII term table support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate clap;
+extern crate term_table;
 extern crate goblin;
 
 use std::path::Path;
@@ -6,6 +7,12 @@ use std::fs::File;
 use std::io::prelude::*;
 
 use clap::{Arg, App};
+
+use term_table::{
+    row::Row,
+    table_cell::{Alignment, TableCell},
+};
+use term_table::{Table, TableStyle};
 
 #[cfg(target_pointer_width = "64")]
 use goblin::elf64 as elf;
@@ -19,7 +26,6 @@ use goblin::Object;
 
 fn main() {
 
-    // TODO: logging ??
     // TODO: emit json or protobuf
     // TODO: additional info
     let matches = App::new("binsec")
@@ -47,13 +53,28 @@ fn main() {
         _                   => { panic!("unsupported binary format"); }
     };
 
+    // initialize ASCII terminal table
+    let mut table = Table::new();
+    table.max_column_width = 40;
+    table.style = TableStyle::extended();
+
+    // header row
+    table.add_row(Row::new(vec![
+        TableCell::new_with_alignment("Security Features", 2, Alignment::Center)
+    ])); 
+
     // check for RELRO
     let relro_headers: Vec<ProgramHeader> = elf.program_headers
         .iter()
         .filter(|ph| program_header::pt_to_str(ph.p_type) == "PT_GNU_RELRO")
         .cloned()
         .collect();
-    println!("{:?}", relro_headers[0]);
+
+    table.add_row(Row::new(vec![
+        TableCell::new("RELRO"),
+        TableCell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+    ]));
+
 
     // check for non-executable stack
     let nx_headers: Vec<ProgramHeader> = elf.program_headers
@@ -61,7 +82,12 @@ fn main() {
         .filter(|ph| program_header::pt_to_str(ph.p_type) == "PT_GNU_STACK")
         .cloned()
         .collect();
-    println!("{:?}", nx_headers[0]);
+
+    table.add_row(Row::new(vec![
+        TableCell::new("NX"),
+        TableCell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+    ]));
+
 
     // check for stack canary
     let strtab = elf.strtab.to_vec().unwrap();
@@ -71,5 +97,5 @@ fn main() {
         }
     }
 
-
+    println!("{}", table.render());
 }
