ec9da5138ef3ee95e6972647a357d3a84fe31e53
ex0dus-0x <ex0dus@codemuch.tech>
2021-04-24 18:05:11 -0400

    Fixes

--- src/format.rs
+++ src/format.rs
@@ -19,7 +19,7 @@ pub fn generate_table(name: &str, mapping: FeatureMap) -> String {
     // initialize blank style term table
     let mut table = Table::new();
     table.max_column_width = 200;
-    table.style = TableStyle::simple();
+    table.style = TableStyle::blank();
 
     // create bolded header
     table.add_row(Row::new(vec![TableCell::new_with_alignment(
@@ -30,10 +30,10 @@ pub fn generate_table(name: &str, mapping: FeatureMap) -> String {
 
     for (name, feature) in mapping {
         let feature_cell = match feature {
-            Value::Bool(true) => TableCell::new_with_alignment("✔️".green(), 1, Alignment::Center),
-            Value::Bool(false) => TableCell::new_with_alignment("✖️".red(), 1, Alignment::Center),
-            Value::String(val) => TableCell::new_with_alignment(val.bold(), 1, Alignment::Center),
-            _ => TableCell::new_with_alignment(feature, 1, Alignment::Center),
+            Value::Bool(true) => TableCell::new_with_alignment("✔️".green(), 1, Alignment::Right),
+            Value::Bool(false) => TableCell::new_with_alignment("✖️".red(), 1, Alignment::Right),
+            Value::String(val) => TableCell::new_with_alignment(val.bold(), 1, Alignment::Right),
+            _ => TableCell::new_with_alignment(feature, 1, Alignment::Right),
         };
 
         table.add_row(Row::new(vec![TableCell::new(name), feature_cell]));
