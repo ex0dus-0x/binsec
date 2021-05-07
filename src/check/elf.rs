5f8556ca30eec2ddb845610f8da26a9f4de7271b
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2021-05-06 23:01:51 -0400

    Cleanup

--- src/check/elf.rs
+++ src/check/elf.rs
@@ -95,10 +95,27 @@ impl Analyze for Elf<'_> {
 
     fn run_instrumentation_checks(&self) -> Option<GenericMap> {
         let mut inst_map = GenericMap::new();
-        let mut has_inst: bool = false;
-        if !has_inst {
-            return None;
+
+        // find symbols for stack canary and FORTIFY_SOURCE
+        for _sym in self.syms.iter() {
+            let _symbol = self.strtab.get(_sym.st_name);
+            if let Some(Ok(symbol)) = _symbol {
+                if symbol.starts_with("__afl") {
+                    inst_map.insert("AFL Instrumentation", json!(true));
+                } else if symbol.starts_with("__asan") {
+                    inst_map.insert("Address Sanitizer", json!(true));
+                } else if symbol.starts_with("__ubsan") {
+                    inst_map.insert("Undefined Behavior Sanitizer", json!(true));
+                } else if symbol.starts_with("__llvm") {
+                    inst_map.insert("LLVM Code Coverage", json!(true));
+                }
+            }
         }
-        Some(inst_map)
+
+        if inst_map.is_empty() {
+            None
+        } else {
+            Some(inst_map)
+        }
     }
 }
